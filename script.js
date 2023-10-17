7a32d49190309a693e7d074c812a9a16661b75b4
Soufian Chakkouri <124874836+soufianchk@users.noreply.github.com>
2023-10-17 16:41:27 +0100

    update the files

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-const appSettings = {
-    databaseURL: "https://shoplist-d2491-default-rtdb.europe-west1.firebasedatabase.app/"
-}
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
-
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
@@ -16,20 +6,25 @@ addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
     if (inputValue.trim() !== "") {
-    push(shoppingListInDB, inputValue)
-    appendItemToShoppingListEl(inputValue)
+        addItemToLocalStorage(inputValue)
+        appendItemToShoppingListEl(inputValue)
     }
     clearInputFieldEl()
 })
+
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
+
 function appendItemToShoppingListEl(itemValue) {
     shoppingListEl.innerHTML += `<li>${itemValue}</li>`
 }
+
 let clearButtonEl = document.getElementById("clear-button")
 clearButtonEl.addEventListener("click", function() {
     shoppingListEl.innerHTML = ""
+    localStorage.removeItem("shoppingList")
+    
 });
 
 let ulEl = document.getElementById("shopping-list")
@@ -37,4 +32,29 @@ ulEl.addEventListener("click", function(event) {
     if (event.target.tagName === "LI") {
         event.target.remove()
     }
+})
+
+// Store the shopping list item in the local storage
+function addItemToLocalStorage(itemValue) {
+    let shoppingList = getShoppingListFromLocalStorage()
+    shoppingList.push(itemValue)
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
+}
+
+// Retrieve the shopping list from the local storage
+function getShoppingListFromLocalStorage() {
+    let shoppingList = localStorage.getItem("shoppingList")
+    if (shoppingList) {
+        return JSON.parse(shoppingList)
+    } else {
+        return []
+    }
+}
+
+// Retrieve the shopping list items from the local storage when the page loads
+window.addEventListener("load", function() {
+    let shoppingList = getShoppingListFromLocalStorage()
+    shoppingList.forEach((itemValue) => {
+        appendItemToShoppingListEl(itemValue)
+    })
 })
